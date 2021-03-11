a75a4d83728244ef407af3ef39e4e7a41435f459
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-11 06:35:48 -0800

    feat: create FastAPI app with tests

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from resume_api.main import app
+
+client = TestClient(app)
+
+
+def test_home_should_return_hello_world():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
