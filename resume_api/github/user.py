76555ec56d11cde076f1fc6996f437ba5e222c5c
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-11 09:26:14 -0800

    feat: Integrate with the GitHub API
    Fixes PFR-16, PFR-14

---
+++ resume_api/github/user.py
@@ -0,0 +1,6 @@
+from typing import TypedDict
+
+
+class GitHubUser(TypedDict):
+    total_private_repos: int
+    owned_private_repos: int
---
+++ tests/test_github_api.py
@@ -0,0 +1,21 @@
+from pytest import mark
+from resume_api.config import settings
+from resume_api.github.api import GitHubAPI
+
+
+class Test_GitHub_API_Integration:
+    class Test_When_not_authenticated:
+        @mark.asyncio
+        async def test_it_cannot_get_my_number_of_private_repos(self):
+            ghAPI = GitHubAPI()
+            actualUserProfile = await ghAPI.GetProfile(settings.GITHUB_USERNAME)
+            assert "total_private_repos" not in actualUserProfile
+
+    class Test_When_authenticated:
+        @mark.asyncio
+        async def test_it_can_get_my_number_of_private_repos(self):
+            ghAPI = GitHubAPI().Authenticate(
+                token=settings.GITHUB_TOKEN, user=settings.GITHUB_USERNAME
+            )
+            actualUserProfile = await ghAPI.GetProfile(settings.GITHUB_USERNAME)
+            assert actualUserProfile["total_private_repos"] > 1
