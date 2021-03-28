988f5d5b37349a4f8851cdf5440d1740771cdc1b
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-27 21:41:16 -0700

    fix: endpoint of '/projects'
    Should be an empty string, otherwise we get temporary redirects (307) to '/projects/' when requesting '/projects'

--- backend/portfolio_api/api_v1/endpoints/projects.py
+++ backend/portfolio_api/api_v1/endpoints/projects.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from fastapi import APIRouter
 from portfolio_api.data.json import JsonDataService
 from portfolio_api.models import Project
@@ -6,6 +7,6 @@ from portfolio_api.models import Project
 router = APIRouter()
 
 
-@router.get("/", response_model=List[Project])
+@router.get("", response_model=List[Project])
 def read_projects():
     return JsonDataService.Projects()
